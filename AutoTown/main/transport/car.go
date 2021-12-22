5e5e8cc870d9a1d4430e65acf2b392233955fd89
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-12-22 16:02:36 +0300

    Selected cell is highlighted, refactoring

--- AutoTown/main/transport/car.go
+++ AutoTown/main/transport/car.go
@@ -16,8 +16,8 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"logo\"\n"
+  data: "tile_set: \"/main/transport/car.atlas\"\n"
+  "default_animation: \"logo_256\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
