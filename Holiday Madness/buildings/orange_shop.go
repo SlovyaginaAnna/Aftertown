b2fe16e89a0410e7e29de49d9c4f7780e8051610
Slovyagina Anna <amslovyagina@edu.hse.ru>
2022-03-16 17:19:28 +0300

    shops

--- Holiday Madness/buildings/orange_shop.go
+++ Holiday Madness/buildings/orange_shop.go
@@ -13,21 +13,6 @@ components {
     w: 1.0
   }
 }
-components {
-  id: "shop1"
-  component: "/scripts/shop.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "label"
   type: "label"
--- Holiday Madness/buildings/red_shop.go
+++ Holiday Madness/buildings/red_shop.go
@@ -1,3 +1,18 @@
+components {
+  id: "shop"
+  component: "/scripts/shop.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "label"
   type: "label"
