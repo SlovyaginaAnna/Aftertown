e4b329c7b9db99f9e59f5ef12281798000945a96
Slovyagina Anna <amslovyagina@edu.hse.ru>
2022-04-09 23:49:58 +0300

    signs panel

--- Holiday Madness/scripts/signs.lua
+++ Holiday Madness/scripts/signs.lua
@@ -4,9 +4,11 @@ local tiles = {{31, 125}, {38, 125}, {65, 125}, {38, 115}, {65, 115}, {28, 92},
 {96, 104}, {96, 113}, {38, 67}, {52, 67}, {65, 78}, {65, 84}, {82, 84}, {108, 84},
 {108, 80}, {118, 80}, {137, 71}, {38, 45}, {45, 45}, {52, 45}, {65, 45}, {82, 45},
 {121, 45}, {137, 45}, {116, 52}, {145, 25}, {115, 25}, {115, 18}, {99, 15},{99, 26},
-{99, 36}, {82, 36}, {82, 26}, {82, 15}, {45, 15}, {31, 2}, {38, 26}, {33, 36}}
+{99, 36}, {82, 36}, {82, 26}, {82, 15}, {45, 15}, {31, 2}, {38, 26}, {38, 36}}
 local tile_size = 16
 local tiles_count = 45
+local sign_types = {'green_brick', 'red_brick', 'green_straight', 'right_straight', 'green_right', 'red_right', 'green_left', 'right_left',
+	'green_not_right', 'red_not_right', 'green_not_left', 'red_not_left', 'traffic_light'}
 
 function S.pos(event_pos)
 	local x = event_pos.x + go.get_position("/camera#camera").x
