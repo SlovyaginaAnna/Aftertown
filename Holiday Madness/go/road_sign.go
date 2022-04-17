f0fc45af15bf1af2aa72ffe728a1fd52880f9873
Slovyagina Anna <amslovyagina@edu.hse.ru>
2022-04-17 23:55:19 +0300

    signs set

---
+++ Holiday Madness/go/road_sign.go
@@ -0,0 +1,255 @@
+components {
+  id: "road_sign"
+  component: "/scripts/road_sign.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "green_brick"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"green_brick\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "red_brick"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"red_brick\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "green_left"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"green_left\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "green_not_left"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"green_not_left\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "green_not_right"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"green_not_right\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "green_right"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"green_right\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "green_straight"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"green_straight\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "red_left"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"red_left\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "red_not_left"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"red_not_left\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "red_not_right"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"red_not_right\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "red_right"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"red_right\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "red_straight"
+  type: "sprite"
+  data: "tile_set: \"/atlases/signs_for_road.atlas\"\n"
+  "default_animation: \"red_straight\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- Holiday Madness/scripts/signs.lua
+++ Holiday Madness/scripts/signs.lua
@@ -1,28 +1,42 @@
 local S = {}
-local tiles = {{{31, 125}, {}}, {{38, 125}, {}}, {{65, 125}, {}}, {{38, 115}, {}}, {{65, 115}, {}}, {{28, 92}, {}},
-{{38, 92}, {}}, {{65, 92}, {}}, {{82, 92}, {}}, {{96, 92}, {}}, {{108, 92}, {}}, {{118, 92}, {}}, {{137, 92}, {}},
-{{96, 104}, {}}, {{96, 113},{}}, {{38, 67}, {}}, {{52, 67}, {}}, {{65, 78}, {}}, {{65, 84}, {}}, {{82, 84}, {}}, {{108, 84}, {}},
-{{108, 80}, {}}, {{118, 80}, {}}, {{137, 71}, {}}, {{38, 45}, {}}, {{45, 45}, {}}, {{52, 45}, {}}, {{65, 45}, {}}, {{82, 45}, {}},
-{{121, 45}, {}}, {{137, 45}, {}}, {{116, 52}, {}}, {{145, 25}, {}}, {{115, 25}, {}}, {{115, 18}, {}}, {{99, 15}, {}},{{99, 26}, {}},
-{{99, 36}, {}}, {{82, 36}, {}}, {{82, 26}, {}}, {{82, 15}, {}}, {{45, 15}, {}}, {{31, 2}, {}}, {{38, 26}, {}}, {{38, 36}, {}}}
+local tiles = {{31, 125}, {38, 125}, {65, 125}, {38, 115}, {65, 115}, {28, 92},
+{38, 92}, {65, 92}, {82, 92}, {96, 92}, {108, 92}, {118, 92}, {137, 92},
+{96, 104}, {96, 113}, {38, 67}, {52, 67}, {65, 78}, {65, 84}, {82, 84}, {108, 84},
+{108, 80}, {118, 80}, {137, 71}, {38, 45}, {45, 45}, {52, 45}, {65, 45}, {82, 45},
+{121, 45}, {137, 45}, {116, 52}, {145, 25}, {115, 25}, {115, 18}, {99, 15}, {99, 26},
+{99, 36}, {82, 36}, {82, 26}, {82, 15}, {45, 15}, {31, 2}, {38, 26}, {38, 36}}
+local signs = {}
 local tile_size = 16
 local tiles_count = 45
 local signs_num = 13
 local sign_types = {'green_brick', 'red_brick', 'green_straight', 'red_straight', 'green_right', 'red_right', 'green_left', 'red_left',
 'green_not_right', 'red_not_right', 'green_not_left', 'red_not_left', 'traffic_light'}
-local quantity = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
-local price = {10, 15, 20, 25, 30, 25, 34, 60, 48, 10, 22, 45, 60}
+local quantity = {}
+local price = {10, 15, 20, 25, 30, 25, 34, 60, 48, 10, 22, 45, 10}
 
-function S.pos(event_pos)
+function S.init()
+	for i = 1, #tiles do
+		table.insert(signs, {0, false, {}})
+	end
+	for i = 1, #sign_types do
+		table.insert(quantity, {sign_types[i], 0, 0})
+	end
+end
+
+function S.tile_index(event_pos)
 	local x = event_pos.x + go.get_position("/camera#camera").x
 	local y = event_pos.y + go.get_position("/camera#camera").y
 	for i = 1, tiles_count  do
-		if x < tiles[i][1][1] * tile_size + tile_size and x > tiles[i][1][1] * tile_size - tile_size * 2 and
-		y < tiles[i][1][2] * tile_size + tile_size and y > tiles[i][1][2] * tile_size - tile_size * 2 then
-			return vmath.vector3(tiles[i][1][1] * tile_size - tile_size / 2, tiles[i][1][2] * tile_size - tile_size / 2, 1)
+		if x < tiles[i][1] * tile_size + tile_size and x > tiles[i][1] * tile_size - tile_size * 2 and
+		y < tiles[i][2] * tile_size + tile_size and y > tiles[i][2] * tile_size - tile_size * 2 then
+			return i
 		end
 	end
-	return nil
+	return 0
+end
+
+function S.pos(index)
+	return vmath.vector3(tiles[index][1] * tile_size - tile_size / 2, tiles[index][2] * tile_size - tile_size / 2, 1)
 end
 
 function S.price(type)
@@ -36,10 +50,10 @@ function S.price(type)
 	return price[index]
 end
 
-function S.notAvailable(currency)
+function S.available(currency)
 	not_available = {}
 	for i = 1, signs_num do
-		if price[i] > currency then
+		if price[i] <= currency then
 			table.insert(not_available, sign_types[i])
 		end
 	end
@@ -51,9 +65,9 @@ function S.allSigns()
 end
 
 function S.buySign(type)
-	for i = 1, signs_num do
-		if type == sign_types[i] then
-			quantity[i][2] = quantity[i][2] + 1
+	for i = 1, #quantity do
+		if type == quantity[i][1] then
+			quantity[i][3] = quantity[i][3] + 1
 			break
 		end
 	end
@@ -70,6 +84,24 @@ end
 
 function S.get_price_table()
 	return price
+end
+
+function S.get_sign_by_index(index)
+	return signs[index]
+end
+
+function S.get_quantity()
+	return quantity
+end
+
+function S.set_sign(type)
+	for i = 1, #quantity do
+		if type == quantity[i][1] and quantity[i][2] < quantity[i][3] then
+			quantity[i][2] = quantity[i][2] + 1
+			return true
+		end
+	end
+	return false
 end
 
 return S
