7f5700a37869f1a2a55be54b11a7eaa92b411a89
Slovyagina Anna <amslovyagina@edu.hse.ru>
2022-05-02 01:02:19 +0300

    day cycle

--- Holiday Madness/scripts/day_cycle.lua
+++ Holiday Madness/scripts/day_cycle.lua
@@ -1,9 +1,15 @@
 local day_cycle = {}
 local day = true
+local training = true
 local day_length = 60
+local already_home = false
 
 function day_cycle.everyone_home()
 	-- TODO
+	if already_home == false and not day and not training then
+		already_home = true
+		open_end_day_panel()
+	end
 	print("Everyone is home")
 end
 
@@ -17,10 +23,15 @@ end
 
 function day_cycle.start_day()
 	day = true
+	already_home = false
 end
 
 function day_cycle.start_night()
 	day = false
+end
+
+function day_cycle.end_training()
+	training = false
 end
 
 return day_cycle
